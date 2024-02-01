3eaf60ad40e99c3cb450a6b4a5ac44ef0280e3e2
farchaab <farid.chaabane@chuv.ch>
2024-02-01 16:40:59 +0100

    fixed camisim tables headers

--- bin/camisim_tables.py
+++ bin/camisim_tables.py
@@ -12,26 +12,25 @@ tsv = sys.argv[1]
 prefix = sys.argv[2]
 
 cami_df = pd.read_csv(tsv, sep="\t")
-cami_df["path"] = [path.replace(".gz", "") for path in cami_df["path"]]
-cami_df["OTU"] = [float(i) + 1 for i in cami_df.index]
-cami_df["genome_ID"] = [f"Genome{i}" for i in cami_df["OTU"]]
+cami_df["OTU"] = cami_df["taxid"]
+cami_df["genome_ID"] = [f"Genome{int(i)}" for i in cami_df.index]
 cami_df["NCBI_ID"] = cami_df["taxid"]
 cami_df["novelty_category"] = ["known_strain"] * len(cami_df)
 
 cami_df[["genome_ID", "OTU", "NCBI_ID", "novelty_category"]].to_csv(
-    f"{prefix}_metadata.tsv",
+    f"metadata_{prefix}.tsv",
     sep="\t",
     index=None,
 )
 cami_df[["genome_ID", "proportion"]].to_csv(
-    f"{prefix}_distribution.txt",
+    f"distribution_{prefix}.tsv",
     sep="\t",
     index=None,
-    header=None,
+    header=False,
 )
 cami_df[["genome_ID", "path"]].to_csv(
-    f"{prefix}_genome_to_id.tsv",
+    f"genome_to_id_{prefix}.tsv",
     sep="\t",
     index=None,
-    header=None,
+    header=False,
 )
