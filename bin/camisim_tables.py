a1f13ca08f7866c98fa4a392c59761d8fe2c7914
farchaab <farid.chaabane@chuv.ch>
2024-09-05 17:03:28 +0200

    added utf8 encoding and fixed indexes

--- bin/camisim_tables.py
+++ bin/camisim_tables.py
@@ -8,7 +8,7 @@ if len(sys.argv) != 3:
     exit("Usage: camisim_tables.py <abundance.tsv> <sample>")
 
 
-df = pd.read_csv(sys.argv[1], sep="\t")
+df = pd.read_csv(sys.argv[1], sep="\t", encoding="utf-8")
 prefix = sys.argv[2]
 
 df["OTU"] = df["tax_id"]
@@ -17,19 +17,11 @@ df["NCBI_ID"] = df["tax_id"]
 df["novelty_category"] = ["known_strain"] * len(df)
 
 df[["genome_ID", "OTU", "NCBI_ID", "novelty_category"]].to_csv(
-    f"metadata_{prefix}.tsv",
-    sep="\t",
-    index=None,
+    f"metadata_{prefix}.tsv", sep="\t", index=False, encoding="utf-8"
 )
 df[["genome_ID", "tax_abundance"]].to_csv(
-    f"distribution_{prefix}.tsv",
-    sep="\t",
-    index=None,
-    header=False,
+    f"distribution_{prefix}.tsv", sep="\t", index=False, header=False, encoding="utf-8"
 )
 df[["genome_ID", "path"]].to_csv(
-    f"genome_to_id_{prefix}.tsv",
-    sep="\t",
-    index=None,
-    header=False,
+    f"genome_to_id_{prefix}.tsv", sep="\t", index=False, header=False, encoding="utf-8"
 )
